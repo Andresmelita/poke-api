2ba8c13634a2d3203defc9ad71e0a07af5b5d600
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2022-10-14 17:14:37 -0300

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div className='pokedex'>
             <h1 className='pokedex_title'>Pokedex</h1>
             <h2 className='pokedex_subtitle'>Hi Trainer!</h2>
-            <p className='pokedex_text'>Give me your to see the pokedex</p>
+            <p className='pokedex_text'>Give me your name to see the pokedex</p>
             <FormHome/>
         </div>
     )
