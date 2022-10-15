a0549a0677ae781d0e5e684db5bf73878a0c6fd4
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2022-10-14 22:34:25 -0300

    changes

--- src/Pages/PokedexByID.jsx
+++ src/Pages/PokedexByID.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const PokedeXByID = () => {
+
+    const{id} = useParams()
+
+    const [pokemon, setPokemon] = useState()
+
+    useEffect(() => {
+        const URL = `https://pokeapi.co/api/v2/pokemon/${id}/`
+        axios.get(URL)
+            .then(res => setPokemon(res.data))
+            .catch(err => console.log(err))
+    }, [])
+
+    console.log(pokemon)
+    
+
     return (
         <div>PokedeXByID</div>
     )
