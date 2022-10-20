d550ec88d1540630439b1e8f8159642e0447f3d1
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2022-10-20 17:08:04 -0300

    changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FormHome from '../components/home/FormHome'
 import './styles/home.css'
 
 const Home = () => {
+
+    //Imagen de fondo
+    const [backImage, setBackImage] = useState()
+
+    useEffect(()=>{
+        const URL = `url(../images/home/deoxis.png)`
+        setBackImage(URL)
+    },)
+
+    const backGroundObject = {
+        backgroundImage: backImage
+    }
+
     return (
-        <div className='pokedex'>
+        <div className='pokedex' style={backGroundObject}>
             <img className='pokedex_img' src='./images/home/pokedex.png' alt=''/>
             <header className='pokedex_header'>
                 <h2 className='pokedex_subtitle'>Hi Trainer!</h2>
