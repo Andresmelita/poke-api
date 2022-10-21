a9606c34f57ece48cd24518c619be3e3243f900a
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2022-10-21 12:21:47 -0300

    changes

--- src/Pages/PokedexByID.jsx
+++ src/Pages/PokedexByID.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Pokemon404 from '../components/pokedexID/Pokemon404'
+import pokedexByID from './styles/pokedexByID.css'
 
 const PokedeXByID = () => {
 
@@ -25,12 +26,60 @@ const PokedeXByID = () => {
     }
 
     return (
-        <article>
-            <header>
-                <img src={pokemon?.sprites.other['official-artwork'].front_default} alt=''/>
-            </header>
-            <section>
-                <h2>{pokemon?.name}</h2>
+        <article >
+            <section className={`containerID back-${pokemon?.types[0].type.name}`}>
+                <img className='pokeball-img' src='./images/pokeball3.png' alt=''/>
+
+                <article className={`card-pokeID border-${pokemon?.types[0].type.name}`}>
+                    <img className='card-poke_spriteID' src={pokemon?.sprites.other['official-artwork'].front_default} alt=''/>
+                    <header className={`card-poke_headerID bg-${pokemon?.types[0].type.name}`}></header>
+
+
+                    <section className='card-poke_bodyID'>
+                        <h3 className={`card-poke_nameID letter-${pokemon?.types[0].type.name}`}>{pokemon?.name}</h3>
+                        <ul className='card-poke_types-container'>
+                            {
+                                pokemon?.types.map(type => (
+                                    <li key={type.slot} className='card-poke_type'>{type.type.name}</li>
+                                ))
+                            }
+                        </ul>
+                        <p className='card-poke_type-label'>Type</p>
+                        <ul className='card-poke_stat-container'>
+                            {
+                                pokemon?.stats.map(stat =>(
+                                    <li key={stat.stat.name} className='card-poke_stat'>
+                                        <span className='card-poke_stat-label'>{stat.stat.name}</span>
+                                        <span className={`card-poke_stat-number letter-${pokemon?.types[0].type.name}`}>{stat.base_stat}</span>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </section>
+                    <section className='card-poke_bodyID'>
+                        <h3 className={`card-poke_nameID letter-${pokemon?.types[0].type.name}`}>{pokemon?.name}</h3>
+                        <ul className='card-poke_types-container'>
+                            {
+                                pokemon?.types.map(type => (
+                                    <li key={type.slot} className='card-poke_type'>{type.type.name}</li>
+                                ))
+                            }
+                        </ul>
+                        <p className='card-poke_type-label'>Type</p>
+                        <ul className='card-poke_stat-container'>
+                            {
+                                pokemon?.stats.map(stat =>(
+                                    <li key={stat.stat.name} className='card-poke_stat'>
+                                        <span className='card-poke_stat-label'>{stat.stat.name}</span>
+                                        <span className={`card-poke_stat-number letter-${pokemon?.types[0].type.name}`}>{stat.base_stat}</span>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </section>
+                </article>
+
+
             </section>
         </article>
     )
