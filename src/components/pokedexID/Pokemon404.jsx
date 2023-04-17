ab757ff7fb865fd3ce974f33949e2464b6bdb4d4
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-04-16 22:36:14 -0400

    background not found modified

--- src/components/pokedexID/Pokemon404.jsx
+++ src/components/pokedexID/Pokemon404.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 import Footer from '../shared/Footer'
 import Header from '../shared/Header'
 import pokemon404 from './styles/pokemon404.css'
+import back from '../../../public/images/back.jpg'
 
 const Pokemon404 = () => {
 
     return (
         <div>
             <Header/>
+            <img className='backImage' src={back}></img>
             <div className='view'>
                 <h1>Pokemon not Found</h1>
                 <img className='notFound' src='./images/notFound.png' alt=''/>
